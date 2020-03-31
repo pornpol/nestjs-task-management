75c009728acc697d4c94e6a1cf760b46551d2124
pornpol <pornpol.w@gmail.com>
2020-03-31 14:38:57 +0700

    Add update task custom validation pipe

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -14,6 +14,7 @@ import { TasksService } from './tasks.service';
 import { Task, TaskStatus } from './tasks.model';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
+import { TaskStatusValidationPipe } from './pipes/task-status-validation.pipe';
 
 @Controller('tasks')
 export class TasksController {
@@ -47,7 +48,7 @@ export class TasksController {
   @Patch('/:id/status')
   updateTask(
     @Param('id') id: string,
-    @Body('status') status: TaskStatus,
+    @Body('status', TaskStatusValidationPipe) status: TaskStatus,
   ): Task {
     return this.tasksService.updateTask(id, status);
   }
