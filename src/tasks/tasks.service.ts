4cd65365ce894a9a9375ec763508fa652e31941a
pornpol <pornpol.w@gmail.com>
2020-04-01 13:40:03 +0700

    Get User's Task

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -18,8 +18,10 @@ export class TasksService {
     return this.taskRepository.getTasks(filterDto, user);
   }
 
-  async getTaskById(id: number): Promise<Task> {
-    const found = await this.taskRepository.findOne(id);
+  async getTaskById(id: number, user: User): Promise<Task> {
+    const found = await this.taskRepository.findOne({
+      where: { id, userId: user.id },
+    });
 
     if (!found) {
       throw new NotFoundException(`Task with ID '${id}' not found.`);
@@ -39,10 +41,10 @@ export class TasksService {
     }
   }
 
-  async updateTask(id: number, status: TaskStatus): Promise<Task> {
-    const task = await this.getTaskById(id);
-    task.status = status;
-    await task.save();
-    return task;
-  }
+  // async updateTask(id: number, status: TaskStatus): Promise<Task> {
+  //   const task = await this.getTaskById(id);
+  //   task.status = status;
+  //   await task.save();
+  //   return task;
+  // }
 }
